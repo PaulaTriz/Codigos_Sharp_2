1e26cf7a14ae007eefc066c214b4e7c945f8aa9f
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-19 20:52:12 -0300

    Add files via upload

---
+++ Excercicio 2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+        double num = double.Parse(Console.ReadLine());
+
+        double dobro = num * 2;
+        Console.WriteLine("O dobro é: " + dobro);
+    }
+}
