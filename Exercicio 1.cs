ca1c63af003e77565b2d39914d327a6658647a0c
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-19 20:48:38 -0300

    Add files via upload

---
+++ Exercicio 1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número: ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double soma = num1 + num2;
+        Console.WriteLine("A soma é: " + soma);
+    }
+}
