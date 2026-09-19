0ec144637644ece20c3a4a06dcc46c6e8a810ba1
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-19 20:56:51 -0300

    Add files via upload

---
+++ Exercicio 3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+        double num = double.Parse(Console.ReadLine());
+
+        double metade = num / 2.0;
+        Console.WriteLine("A metade é: " + metade);
+    }
+}
+
