09b1464267b4a2075f50800fed327af4bced10a6
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-20 16:34:10 -0300

    Add files via upload

---
+++ Excercicio 6.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] agrs)
+    {
+        Console.Write("Digite um número: ");
+        double num = double.Parse(Console.ReadLine());
+
+        double raiz = Math.Sqrt(num);
+        Console.WriteLine("A raiz quadrada é: " + raiz);
+    }
+}
