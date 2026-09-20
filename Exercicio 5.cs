004bdefdd8c6da42115d65c9123287b11d63ec15
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-19 21:04:47 -0300

    Add files via upload

---
+++ Exercicio 5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o primeiro número (dividendo): ");
+        double num1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número (divisor): ");
+        double num2 = double.Parse(Console.ReadLine());
+
+        double divisao = num1 / num2;
+        Console.WriteLine("O resultado da divisão é: " + divisao);
+    }
+}
+
