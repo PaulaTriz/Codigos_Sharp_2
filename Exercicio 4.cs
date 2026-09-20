4b0e69bf06ab4e7211ddae56fc1ab6f47e607eda
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-19 21:00:28 -0300

    Add files via upload

---
+++ Exercicio 4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número: ");
+        double num = double.Parse(Console.ReadLine());
+
+        double quadrado = Math.Pow(num, 2);
+        Console.WriteLine("O quadrado é: " + quadrado);
+    }
+}
